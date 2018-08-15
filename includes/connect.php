88b2118c719e353b5e148c5598207f8e5621a495
Jessica Klein <jess.klein@live.com>
2018-08-15 19:46:37 -0400

    Initial commit
    PHP-based to-do list

---
+++ includes/connect.php
@@ -0,0 +1,9 @@
+<?php
+    $server = "localhost";
+    $db_user = "root";
+    $db_pass = "Leaf@1743";
+    $db_name = "mysql";
+    
+    mysqli = new mysqli($server, $db_user, $db_pass, $db_name) or die("Could not connect to the database");
+
+?>
