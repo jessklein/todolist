06fc3124aeee2bfbe9f3e179543c3c4adc6c7313
Jessica Klein <jess.klein@live.com>
2018-08-16 00:18:50 -0400

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
     $server = "localhost";
     $db_user = "root";
-    $db_pass = "Leaf@1743";
+    $db_pass = "password";
     $db_name = "mysql";
     
     mysqli = new mysqli($server, $db_user, $db_pass, $db_name) or die("Could not connect to the database");
